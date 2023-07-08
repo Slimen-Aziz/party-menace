9f71a757271e58057c38f7dac1d8da70b4468ee4
AnisLahouar <38791856+AnisLahouar@users.noreply.github.com>
2023-07-08 21:08:40 +0100

    feat: add assets and create cover movement

---
+++ Assets/Scripts/MiniGames/HairPick/Shaver.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace MiniGames.HairPick
+{
+    public class ElectricRazor : MonoBehaviour
+    {
+        [SerializeField] private float coverSpeed;
+        [SerializeField] private float xRange;
+        [SerializeField] private Sprite onSprite;
+        [SerializeField] private Sprite offSprite;
+
+        [SerializeField] private Transform bladeCover;
+        private Transform _transform;
+        [SerializeField] private bool _isOn;
+        private float _direction = 1;
+
+        public void Init()
+        {
+            _transform = transform;
+            _isOn = true;
+        }
+
+        private void Update()
+        {
+            if(!_isOn) return;
+            var newPosition = bladeCover.localPosition;
+            newPosition.x += _direction * coverSpeed * Time.deltaTime;
+            bladeCover.localPosition = newPosition;
+            if (newPosition.x >= xRange) _direction = -1;
+            else if (newPosition.x < -xRange) _direction = 1;
+        }
+    }
+}
