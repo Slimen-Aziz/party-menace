d45d8f2519024277bc64603e4475ed35b6e165bd
Aziz Slimen <42971797+Slimen-Aziz@users.noreply.github.com>
2023-07-08 19:55:59 +0100

    Update DisconnectGame.cs

--- Assets/Scripts/DisconnectGame.cs
+++ Assets/Scripts/DisconnectGame.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DisconnectGame : GameBase
@@ -7,17 +5,12 @@ public class DisconnectGame : GameBase
     [SerializeField] private DraggableElement element;
     [SerializeField] private DraggableHolder elementHolder;
     [SerializeField] private Cable cable;
-    
-    protected override void Start()
+
+    public override void OnStart()
     {
         elementHolder.OnConnect += cable.DisableJoints;
         elementHolder.OnDisconnect += cable.EnableJoints;
         if (element == null) return;
         elementHolder.AttachElement(element);
-    }
-
-    protected override void Update()
-    {
-        
     }
 }
