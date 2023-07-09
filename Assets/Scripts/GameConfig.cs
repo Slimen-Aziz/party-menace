541f1cb4cd58c428d48bfa16d99b7240fc2452c6
AnisLahouar <38791856+AnisLahouar@users.noreply.github.com>
2023-07-09 14:47:14 +0100

    feat: add transitions and fixes to minigames

--- Assets/Scripts/GameConfig.cs
+++ Assets/Scripts/GameConfig.cs
@@ -1,6 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable] public struct GameData
+{
+    public GameBase prefab;
+    public string hint;
+}
 
 [CreateAssetMenu(fileName = "GameConfig", menuName = "GameData/GameConfig", order = 1)]
 public class GameConfig : ScriptableObject
@@ -12,20 +20,23 @@ public class GameConfig : ScriptableObject
     [Header("Shared Configuration")]
     public float baseDuration;
     [Header("MiniGames")]
-    public List<GameBase> availableGames;
+    public List<GameData> availableGames;
 
-    public GameBase RandomGame(GameBase exclusion)
+    public (string, GameBase) RandomGame(GameBase exclusion)
     {
-        if (availableGames == null || availableGames.Count == 0) return null;
+        if (availableGames == null || availableGames.Count == 0) return ("No Game", null);
         var miniGames = availableGames.ToList();
         if (exclusion)
         {
-            var target = miniGames.FirstOrDefault(item => item.GetType() == exclusion.GetType() );
+            var target = miniGames.FirstOrDefault(item => item.prefab.GetType() == exclusion.GetType() );
             // var message = target ? target.GetType().ToString() : "Null";
             // Debug.Log($"{message} == {exclusion.GetType()}");
             miniGames.Remove(target);
         }
-        var result = Instantiate(miniGames[Random.Range(0, miniGames.Count - 1)]);
-        return result;
+
+        var miniGame = miniGames[Random.Range(0, miniGames.Count - 1)];
+        var result = Instantiate(miniGame.prefab);
+        
+        return (miniGame.hint, result);
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,29 +1,123 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button startButton;
-    private Canvas _canvas;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private Ease tweenType;
+    [SerializeField] private RectTransform healthHolder;
+    [SerializeField] private RectTransform[] healthIcons;
+
+    [SerializeField] private Vector2 offScreenPosition;
+    [SerializeField] private Vector2 onScreenPosition;
+
+    [SerializeField] private float movementDuration;
+    [SerializeField] private float healthBlinkingInterval;
+    [SerializeField] private float healthBlinkingAmount;
+
+    [SerializeField] private TextMeshProUGUI hint;
     
+    private int _index;
+
     public void Init()
     {
-        _canvas = GetComponent<Canvas>();
         startButton.onClick.AddListener(StartGame);
+        TransitionOut(null);
+        hint.text = "";
     }
 
     private void StartGame()
     {
         GameController.LoadNextGame();
     }
-    
+
     public void OpenMenu()
     {
-        _canvas.enabled = true;
+        startButton.gameObject.SetActive(true);
     }
 
     public void CloseMenu()
     {
-        _canvas.enabled = false;
+        startButton.gameObject.SetActive(false);
+    }
+
+    public void TransitionOut(Action inCallback = null)
+    {
+        canvasGroup.DOFade(0, fadeDuration).OnComplete(() =>
+        {
+            canvasGroup.interactable = true;
+            hint.text = "";
+            inCallback?.Invoke();
+        });
+    }
+
+    public void TransitionIn(Action inCallback)
+    {
+        canvasGroup.interactable = false;
+        canvasGroup.DOFade(1, fadeDuration).OnComplete(() =>
+        {
+            inCallback?.Invoke();
+        });
+    }
+
+    public void ShowHealth(Action inCallback)
+    {
+        healthHolder.DOAnchorPos(onScreenPosition, movementDuration).SetEase(tweenType).OnComplete(() =>
+        {
+            inCallback?.Invoke();
+        });
+    }
+
+    public void LoseHealth(Action inCallback)
+    {
+        ShowHealth(() =>
+        {
+            LoseHealthAnimated(inCallback);
+        });
+    }
+
+    private void LoseHealthAnimated(Action inCallback)
+    {
+        StartCoroutine(_LoseHealth());
+        
+        IEnumerator _LoseHealth()
+        {
+            var waitForSeconds = new WaitForSeconds(healthBlinkingInterval);
+            var counter = 0;
+            var status = false;
+            
+            inCallback?.Invoke();
+            while (counter < healthBlinkingAmount)
+            {
+                healthIcons[_index].gameObject.SetActive(status);
+                status = !status;
+                counter++;
+                yield return waitForSeconds;
+            }
+            healthIcons[_index].gameObject.SetActive(false);
+            
+            yield return waitForSeconds;
+            _index++;
+            HideHealth(null);
+        }
+    }
+
+    public void HideHealth(Action inCallback)
+    {
+        healthHolder
+            .DOAnchorPos(offScreenPosition, movementDuration)
+            .SetEase(tweenType)
+            .OnComplete(GameController.StartGame);
+    }
+
+    public void ShowHint(string inText)
+    {
+        hint.text = inText;
     }
 }
